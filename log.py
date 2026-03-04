6c375832e9caef0917f0551c0d967f758867f708
Ville Pakarinen <151925563+vpakarinen2@users.noreply.github.com>
2026-03-04 14:11:38 +0200

    Updated main log file

--- log.py
+++ log.py
@@ -3,8 +3,29 @@ import sys
 import os
 
 
+class ColoredConsoleFormatter(logging.Formatter):
+    """Custom formatter to add ANSI colors to console logs."""
+    BRIGHT_WHITE = '\033[97m'
+    BRIGHT_YELLOW = '\033[93m'
+    BRIGHT_RED = '\033[91m'
+    RESET = '\033[0m'
+
+    FORMATS = {
+        logging.INFO: BRIGHT_WHITE + "%(asctime)s - %(name)s - %(levelname)s - %(message)s" + RESET,
+        logging.WARNING: BRIGHT_YELLOW + "%(asctime)s - %(name)s - %(levelname)s - %(message)s" + RESET,
+        logging.ERROR: BRIGHT_RED + "%(asctime)s - %(name)s - %(levelname)s - %(message)s" + RESET,
+        logging.CRITICAL: BRIGHT_RED + "%(asctime)s - %(name)s - %(levelname)s - %(message)s" + RESET,
+        logging.DEBUG: RESET + "%(asctime)s - %(name)s - %(levelname)s - %(message)s" + RESET
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno, self.FORMATS[logging.INFO])
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def setup_logger(name="GraniteAgent", log_file="agent.log", level=logging.INFO):
-    """Initializes and returns a customized logger for the agent."""
+    """Initializes and returns customized logger."""
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
@@ -15,17 +36,14 @@ def setup_logger(name="GraniteAgent", log_file="agent.log", level=logging.INFO):
     if log_dir and not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    log_format = logging.Formatter(
-        fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-
+    file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     file_handler = logging.FileHandler(log_file, mode='a', encoding='utf-8')
     file_handler.setLevel(level)
-    file_handler.setFormatter(log_format)
+    file_handler.setFormatter(file_format)
 
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setLevel(level)
-    console_handler.setFormatter(log_format)
+    console_handler.setFormatter(ColoredConsoleFormatter())
 
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
@@ -33,4 +51,3 @@ def setup_logger(name="GraniteAgent", log_file="agent.log", level=logging.INFO):
     return logger
 
 agent_logger = setup_logger()
-
